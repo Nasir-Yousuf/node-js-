871d000a4aab3be7ca475f8a1fa652d4ba43b804
NasirYousuf <102212241+Nasir-Yousuf@users.noreply.github.com>
2022-09-10 12:57:48 +0600

    files modified

--- node.js/server.js
+++ node.js/server.js
@@ -1,25 +1,40 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+
+console.log(process.env.DATABASE);
+
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  '<password>',
   process.env.DATABASE_PASSWORD
 );
 
 mongoose
   .connect(DB, {
-    useUnifiedTopology: true,
+    dbName: 'natours-app',
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
   })
-  .then(() => console.log('db Connection successful'));
+  .then(() => console.log('DB connection successful!'));
 
-const port = 8000;
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
 
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
