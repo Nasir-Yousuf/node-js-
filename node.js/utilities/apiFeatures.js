e6998a83a884bb3976aae0015801956fdf8dc224
NasirYousuf <102212241+Nasir-Yousuf@users.noreply.github.com>
2022-09-10 13:01:09 +0600

    Add files via upload

--- node.js/utilities/apiFeatures.js
+++ node.js/utilities/apiFeatures.js
@@ -1,4 +1,3 @@
-/* eslint-disable node/no-unsupported-features/es-syntax */
 class APIFeatures {
   constructor(query, queryString) {
     this.query = query;
@@ -7,36 +6,33 @@ class APIFeatures {
 
   filter() {
     const queryObj = { ...this.queryString };
-    const exclutedFields = ['page', 'sort', 'limit', 'fields'];
-    exclutedFields.forEach(el => delete queryObj[el]);
-    // console.log(queryObj, req.query);
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
-    let queryObjStr = JSON.stringify(queryObj);
-    queryObjStr = queryObjStr.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      match => `$${match}`
-    );
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    this.query = this.query.find(JSON.parse(queryObjStr));
+    this.query = this.query.find(JSON.parse(queryStr));
 
     return this;
   }
 
   sort() {
     if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' '); //the coma will gone and became a full string
+      const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
       this.query = this.query.sort('-createdAt');
     }
 
-    return this; //return the entire object
+    return this;
   }
 
   limitFields() {
     if (this.queryString.fields) {
-      const fieldsBy = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fieldsBy);
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
     } else {
       this.query = this.query.select('-__v');
     }
@@ -48,10 +44,10 @@ class APIFeatures {
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
+
     this.query = this.query.skip(skip).limit(limit);
 
     return this;
   }
 }
-
 module.exports = APIFeatures;
